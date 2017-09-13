f885cbb2631de7c783ad0a538ca523f534e38c49
Michael Jett <jumbojett@users.noreply.github.com>
2017-09-13 15:29:39 -0400

    Update wrapper.sh

--- assets/wrapper.sh
+++ assets/wrapper.sh
@@ -4,16 +4,16 @@ if [[ ! -d /etc/scalr-server ]]; then
     mkdir -p /etc/scalr-server
 fi
 if [[ ! -d /opt/scalr-server ]]; then
-    mkdir -p /opt/scalr-server
+    mkdir -p /opt/scalr-server/var/log/supervisor
 fi
 if [[ ! -f /etc/scalr-server/scalr-server.rb ]]; then
     echo "BOOTSTRAP - Generating default settings"
-    mv /etc/scalr-server-template/scalr-server.rb /etc/scalr-server/scalr-server.rb
+    cp /etc/scalr-server-template/scalr-server.rb /etc/scalr-server/scalr-server.rb
     chmod 644 /etc/scalr-server/scalr-server.rb
 fi
 if [[ ! -f /etc/scalr-server/scalr-server-secrets.json ]]; then
     echo "BOOTSTRAP - Setting initial credentials"
-    mv /etc/scalr-server-template/scalr-server-secrets.json /etc/scalr-server/scalr-server-secrets.json
+    cp /etc/scalr-server-template/scalr-server-secrets.json /etc/scalr-server/scalr-server-secrets.json
     chmod 600 /etc/scalr-server/scalr-server-secrets.json
 
     ADMINPW=$(grep "admin_password" /etc/scalr-server/scalr-server-secrets.json |awk -F \" '{print $4}')
@@ -21,7 +21,7 @@ if [[ ! -f /etc/scalr-server/scalr-server-secrets.json ]]; then
 fi
 if [[ ! -f /opt/scalr-server/version-manifest.json ]]; then
     echo "BOOTSTRAP - Populating scalr-server data directory"
-    mv /opt/scalr-server-template/* /opt/scalr-server
+    cp /opt/scalr-server-template/* /opt/scalr-server
 fi
 
 /opt/scalr-server/embedded/bin/python /opt/scalr-server/embedded/bin/supervisord -c /opt/scalr-server/etc/supervisor/supervisord.conf -n
